9f123ccc82d6f1a1f3a42c0b682a2d30438a2b79
RomDmitriy <aisek72@gmail.com>
2023-05-30 00:23:12 +0300

    Фикс нерабочего скрипта

--- src/Discord.py
+++ src/Discord.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 from os import path
 import os
 import time
@@ -7,7 +8,7 @@ import time
 
 class Discord:
 
-    isTest = True
+    isTest = False
     driver = None
 
     def __init__(self):
@@ -33,36 +34,38 @@ class Discord:
         return True
 
     def search_users(self):
-        time.sleep(1)
-        self.driver.find_element("div[class^='tabBar'] div:nth-child(2)").click()
+        time.sleep(5)
+        self.driver.find_element(By.XPATH, '//*[@id="app-mount"]/div[2]/div[1]/div[1]/div/div[2]/div/div/div/div/main/section/div[1]/div[4]/div[2]').click()
 
         # Get user tags
-        users_blocks = self.driver.find_element(
-            "div[class^='peopleColumn'] div[class^='peopleListItem']")
+        # users_blocks = self.driver.find_elements(By.CLASS_NAME, 'peopleListItem-u6dGxF')
 
         # Ask for confirmation
-        print(str(len(users_blocks)) + " users were found")
-        answer = input("Are you sure you want to deleted them? [y/n]\n")
-        if not answer or answer[0].lower() != 'y':
-            print('You did not indicate approval')
-            self.exit()
+        # print(str(len(users_blocks)) + " users were found")
+        # answer = input("Are you sure you want to deleted them? [y/n]\n")
+        # if not answer or answer[0].lower() != 'y':
+        #     print('You did not indicate approval')
+        #     self.exit()
 
         # Loop through users blocks
-        for userBlock in users_blocks:
-            userBlock.find_element("div[class^='actions'] div:nth-child(2)").click()
-            time.sleep(0.1)
-            self.driver.find_element("#friend-row-remove-friend").click()
-            time.sleep(0.1)
+        i = 1
+        while True:
+            try:
+                self.driver.find_elements(By.CLASS_NAME, 'actionButton-3-B2x-')[i].click()
+                time.sleep(0.1)
+                self.driver.find_element(By.CLASS_NAME, 'colorDanger-3umuSx').click()
+                time.sleep(0.1)
 
-            # Set isTest variable to test the code
-            if self.isTest:
-                self.driver.find_element("div[class^='layerContainer'] button[type='button']").click()
-            else:
-                self.driver.find_element("div[class^='layerContainer'] button[type='submit']").click()
+                # Set isTest variable to test the code
+                if self.isTest:
+                    self.driver.find_element(By.XPATH, '//*[@id="app-mount"]/div[2]/div[1]/div[3]/div[2]/div/div/div[3]/button[2]').click()
+                else:
+                    self.driver.find_element(By.XPATH, '//*[@id="app-mount"]/div[2]/div[1]/div[3]/div[2]/div/div/div[3]/button[1]').click()
 
-            time.sleep(0.1)
+                time.sleep(0.5)
+            except:
+                pass
 
     def exit(self):
         print("Exiting program...")
         self.driver.quit()
-        exit(1)
